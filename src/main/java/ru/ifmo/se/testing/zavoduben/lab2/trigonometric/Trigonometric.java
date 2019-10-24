bd5d74e2918c59b5f98c22b44a1ce318fd06daba
Andrey Zavodov <a.p.zavodov@gmail.com>
2019-10-24 15:26:33 +0300

    Implement Trigonometric class with necessary methods

---
+++ src/main/java/ru/ifmo/se/testing/zavoduben/lab2/trigonometric/Trigonometric.java
@@ -0,0 +1,35 @@
+package ru.ifmo.se.testing.zavoduben.lab2.trigonometric;
+
+public class Trigonometric {
+
+    private Sine sine;
+
+    public Trigonometric(int numTerms) {
+        this.sine = new Sine(numTerms);
+    }
+
+    public double sin(double x) {
+        return this.sine.applyAsDouble(x);
+    }
+
+    public double cos(double x) {
+        return 1.0 - 2.0 * Math.pow(this.sin(x / 2.0), 2.0);
+    }
+
+    public double sec(double x) {
+        return 1.0 / this.cos(x);
+    }
+
+    public double csc(double x) {
+        return 1.0 / this.sin(x);
+    }
+
+    public double tan(double x) {
+        return this.sin(x) / this.cos(x);
+    }
+
+    public double cot(double x) {
+        return 1.0 / this.tan(x);
+    }
+
+}
